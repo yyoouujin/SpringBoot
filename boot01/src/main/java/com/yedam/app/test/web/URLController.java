356e268d87ce63af70ba4f7763653987aaa34531
= <=>
2024-08-06 17:50:29 +0900

    get 수정

--- boot01/src/main/java/com/yedam/app/test/web/URLController.java
+++ boot01/src/main/java/com/yedam/app/test/web/URLController.java
@@ -1,6 +1,7 @@
 package com.yedam.app.test.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,7 +9,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller //Bean 등록, Web 과 관련된 부분
 public class URLController {
 	
-	@RequestMapping(path="/test", method=RequestMethod.GET)
+	
+	//@RequestMapping(path="/test", method=RequestMethod.GET)
+	@GetMapping("/test")
 	@ResponseBody
 	public String urlGetTest(String keyword) {
 		return "Server Response : Get Method\n Select - " + keyword;
